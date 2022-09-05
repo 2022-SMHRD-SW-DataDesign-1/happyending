11037098f4b2fa7f19344e220579f9d81ef8413d
smhrd <smhrd@DESKTOP-C1K9NNB>
2022-09-05 16:59:05 +0900

    실험용 commit

--- 행복해죽죠/src/행복해죽죠/Main.java
+++ 행복해죽죠/src/행복해죽죠/Main.java
@@ -1,10 +1,10 @@
-package Çàº¹ÇØÁ×ÁÒ;
+package í–‰ë³µí•´ì£½ì£ ;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("git Å×½ºÆ® ÁßÀÔ´Ï´Ù.");
-		System.out.println("ÀÌ¼ºÀç ¹Ùº¸");
+		System.out.println("git í…ŒìŠ¤íŠ¸ ì¤‘ìž…ë‹ˆë‹¤.");
+		System.out.println("ï¿½Ì¼ï¿½ï¿½ï¿½ ï¿½Ùºï¿½");
 
 	}
 
